255a5ee8e7aa85b87b0949f794b054435a986677
angelinatulusa <ang.tulusa@gmail.com>
2022-01-23 19:56:23 +0200

    Добавьте файлы проекта.

---
+++ slovarik1_tulusa/module1.py
@@ -0,0 +1,24 @@
+﻿def loe_failist(f:str,s:list):
+    """Info failist f listisse l
+    """
+    fail=open(f,'r',encoding="utf-8-sig")
+    for rida in fail:
+        s.append(rida.strip())
+    fail.close()
+    return s
+def perevod(rus:list,ang:list):
+    s=input("Какое слово перевести? ")
+    if s in rus:
+        p=ang[rus.index(s)]
+        print(s+"-"+p)
+    elif s in ang:
+        p=rus[ang.index(s)]
+        print(s+"-"+p)
+    else:
+        print("Такого слова в словаре нету")
+def add_slovo(f:str, v:str):
+    with open(f,"a",encoding="utf-8-sig") as fail:
+        fail.write(v+"\n")
+
+
+
---
+++ slovarik1_tulusa/slovarik1_tulusa.py
@@ -0,0 +1,26 @@
+﻿from module1 import *
+rus_list=[]
+ang_list=[]
+rus_list=loe_failist("rus.txt",rus_list)
+ang_list=loe_failist("ang.txt",ang_list)
+try:
+    a=int(input("Это словарик с русского на английский и с английского на русский, хочешь начать перевод?(1-ДА,2-нет) "))
+    if a==1:
+        while True:
+            print("\nЧто ты хочешь сделать? \nУвидеть список всех слов - 1 \nПеревеcти - 2 \nДобавить слово в словарь - 3 \nИсправить ошибку - 4")
+            b=int(input("Выбери чифру "))
+            if b==1:
+                print(rus_list)
+                print(ang_list)
+            elif b==2:
+                perevod(rus_list,ang_list)
+            elif b==3:
+                add_slovo("rus.txt",input("Введите новое слово на русском "))
+                add_slovo("ang.txt",input("Введите новое слово на английском "))
+
+
+    elif a==2:
+        print("Хорошо, до свидания!")
+except:
+    ValueError
+
