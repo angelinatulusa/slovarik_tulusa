1463ba54060bfda3f6c563e5a0bfb1b133d2224d
angelinatulusa <93324341+angelinatulusa@users.noreply.github.com>
2022-01-24 17:49:49 +0200

    Update slovarik1_tulusa.py

--- slovarik1_tulusa/slovarik1_tulusa.py
+++ slovarik1_tulusa/slovarik1_tulusa.py
@@ -1,4 +1,4 @@
-﻿from module1 import *
+from module1 import *
 rus_list=[]
 ang_list=[]
 rus_list=loe_failist("rus.txt",rus_list)
@@ -8,15 +8,20 @@ try:
     if a==1:
         while True:
             print("\nЧто ты хочешь сделать? \nУвидеть список всех слов - 1 \nПеревеcти - 2 \nДобавить слово в словарь - 3 \nИсправить ошибку - 4")
-            b=int(input("Выбери чифру "))
-            if b==1:
-                print(rus_list)
-                print(ang_list)
-            elif b==2:
-                perevod(rus_list,ang_list)
-            elif b==3:
-                add_slovo("rus.txt",input("Введите новое слово на русском "))
-                add_slovo("ang.txt",input("Введите новое слово на английском "))
+            try:
+                b=int(input("Выбери чифру "))
+                if b==1:
+                    print(rus_list)
+                    print(ang_list)
+                elif b==2:
+                    perevod(rus_list,ang_list)
+                elif b==3:
+                    add_slovo("rus.txt",input("Введите новое слово на русском "))
+                    add_slovo("ang.txt",input("Введите новое слово на английском "))
+                elif b==4:
+                    print("к сожалению данная функция недоступна")#ne pridumala kak eto sdelat
+            except:
+                ValueError
 
 
     elif a==2:
