d7e992bd5c5bf5fa5c57e933550f8617ca8df6ba
angelinatulusa <ang.tulusa@gmail.com>
2022-02-01 19:31:04 +0200

    вроде все!

--- slovarik1_tulusa/module1.py
+++ slovarik1_tulusa/module1.py
@@ -1,4 +1,7 @@
-﻿def loe_failist(f:str,s:list):
+﻿from random import shuffle
+import random
+
+def loe_failist(f:str,s:list):
     """Info failist f listisse l
     """
     fail=open(f,'r',encoding="utf-8-sig")
@@ -6,19 +9,55 @@
         s.append(rida.strip())
     fail.close()
     return s
+
 def perevod(rus:list,ang:list):
-    s=input("Какое слово перевести? ")
-    if s in rus:
-        p=ang[rus.index(s)]
-        print(s+"-"+p)
-    elif s in ang:
-        p=rus[ang.index(s)]
-        print(s+"-"+p)
+    slovo=input("Какое слово перевести? ")
+    if slovo in rus:
+        perevod=ang[rus.index(slovo)]
+        print(slovo+"-"+perevod)
+    elif slovo in ang:
+        perevod=rus[ang.index(slovo)]
+        print(slovo+"-"+perevod)
     else:
         print("Такого слова в словаре нету")
+
 def add_slovo(f:str, v:str):
     with open(f,"a",encoding="utf-8-sig") as fail:
         fail.write(v+"\n")
+
+def ispravlenie(slovo:str,l:list):
+    for i in range(len(l)):
+        if l[i]==slovo:
+            new_slovo=slovo.replace(slovo,input("Новое слово: "))
+            l.insert(i,new_slovo)
+            l.remove(slovo)
+    return l
+
+def kontrol(l1:list,l2:list):
+    vsego_ballov=0
+    lists=[]
+    lists.extend(l1)
+    lists.extend(l2)
+    random.shuffle(lists)
+    print('random list ',lists)
+    for i in range(len(l1)):
+        otvet=input(f"Переведи данное слово - '{lists[i]}': ")
+        if otvet in l1 or otvet in l2:
+            if lists[i] in l1:
+               if l1.index(lists[i])==l2.index(otvet):
+                    vsego_ballov+=1
+                    print("Правильно!")
+                    print()
+            elif lists[i] in l2:
+                if l2.index(lists[i])==l1.index(otvet):
+                    vsego_ballov+=1
+                    print("Правильно!")
+                    print()
+        else:
+            print("Неправильно!")
+            print()
+    resultat=(vsego_ballov/len(l1))*100
+    print(f"Ваш результат: {resultat}%")
 
 
 
--- slovarik1_tulusa/slovarik1_tulusa.py
+++ slovarik1_tulusa/slovarik1_tulusa.py
@@ -7,7 +7,7 @@ try:
     a=int(input("Это словарик с русского на английский и с английского на русский, хочешь начать перевод?(1-ДА,2-нет) "))
     if a==1:
         while True:
-            print("\nЧто ты хочешь сделать? \nУвидеть список всех слов - 1 \nПеревеcти - 2 \nДобавить слово в словарь - 3 \nИсправить ошибку - 4")
+            print("\nЧто ты хочешь сделать? \nУвидеть список всех слов - 1 \nПеревеcти - 2 \nДобавить слово в словарь - 3 \nКонтроль на знание слов - 4  \nИсправить ошибку - 5 \nХочу закончить - 6")
             try:
                 b=int(input("Выбери чифру "))
                 if b==1:
@@ -19,13 +19,24 @@ try:
                     add_slovo("rus.txt",input("Введите новое слово на русском "))
                     add_slovo("ang.txt",input("Введите новое слово на английском "))
                 elif b==4:
-                    print("К сожалению данная функция недоступна")#ne pridumala kak eto sdelat
+                    kontrol(ang_list,rus_list)
+                elif b==5:
+                    jazik=int(input("В каком языке ошибка? (1-русский, 2-английский) "))
+                    if jazik==1:
+                        rus_list=ispravlenie(input("Слово: "),rus_list)
+                        failisse(rus_list,"rus.txt")
+                    elif jazik==2:
+                        ang_list=ispravlenie(input("Word: "),ang_list)
+                        failisse(ang_list,"ang.txt")
+                    else:
+                        print("Такого варианта нету!")
+                elif b==6:
+                    print("Хорошо, до свидания!")
+                    break
                 else:
                     print("Такого варианта нету!")
             except:
                 ValueError
-
-
     elif a==2:
         print("Хорошо, до свидания!")
 except:
